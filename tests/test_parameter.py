c0ca13495260cbaba4d40b80c99996bb41d1af00
Chan-Ho Suh <chanhosuh@gmail.com>
2023-08-17 21:17:12 -0400

    Complete the test with asserts after ramp finish

--- tests/test_parameter.py
+++ tests/test_parameter.py
@@ -10,14 +10,19 @@ from curve_dao.vote_utils import make_vote
 
 @pytest.fixture(scope="module")
 def tricrypto_pool():
+    # TriCryptoINV
     yield ape.Contract("0x5426178799ee0a0181a89b4f57efddfab49941ec")
 
 
 def test_parameter_update(vote_deployer, tricrypto_pool):
-    # TriCryptoINV
+    week_seconds = 7 * 86400
+    block = ape.chain.blocks[-1]
+    last_timestamp = block.timestamp
+
     future_A = 1707650
     future_gamma = 11809167829000
-    future_time = 1693011177
+    future_time = last_timestamp + 2 * week_seconds
+
     parameter_action = (
         tricrypto_pool.address,
         "ramp_A_gamma",
@@ -25,6 +30,9 @@ def test_parameter_update(vote_deployer, tricrypto_pool):
         future_gamma,
         future_time,
     )
+
+    assert tricrypto_pool.A() != future_A
+    assert tricrypto_pool.gamma() != future_gamma
 
     tx = make_vote(
         target=CURVE_DAO_OWNERSHIP,  # tricrypto-ng factory admin is OWNERSHIP agent
@@ -37,10 +45,12 @@ def test_parameter_update(vote_deployer, tricrypto_pool):
         vote_id = log.event_arguments["voteId"]
         break
 
-    return
     simulate(
         vote_id=vote_id,
         voting_contract=CURVE_DAO_OWNERSHIP["voting"],
     )
 
-    # assert the parameter has updated
+    ape.chain.mine(deltatime=week_seconds)
+
+    assert tricrypto_pool.A() == future_A
+    assert tricrypto_pool.gamma() == future_gamma
