2737ee935d4ecf38132e9081be0bef52b1aa0209
Chan-Ho Suh <chanhosuh@gmail.com>
2023-08-18 14:28:36 -0400

    Add parameter tests for crypto 2-coin factory

--- tests/test_parameter.py
+++ tests/test_parameter.py
@@ -1,9 +1,15 @@
 import ape
 import pytest
 
-from curve_dao import CURVE_DAO_OWNERSHIP
+from curve_dao import CRYPTOSWAP_OWNER_PROXY, CURVE_DAO_OWNERSHIP, CURVE_DAO_PARAM
 from curve_dao.simulate import simulate
 from curve_dao.vote_utils import make_vote
+
+
+@pytest.fixture(scope="module")
+def crypto_factory_pool():
+    # JPEG/ETH
+    yield ape.Contract("0x7e050cf658777cc1da4a4508e79d71859044b60e")
 
 
 @pytest.fixture(scope="module")
@@ -14,7 +20,52 @@ def tricrypto_pool():
     # yield ape.Contract("0x4ebdf703948ddcea3b11f675b4d1fba9d2414a14")
 
 
-def test_ramp_parameters(vote_deployer, tricrypto_pool):
+def test_crypto_factory_ramp_parameters(vote_deployer, crypto_factory_pool):
+    week_seconds = 7 * 86400
+    block = ape.chain.blocks[-1]
+    last_timestamp = block.timestamp
+
+    future_A = 500000
+    future_gamma = 155000000000000
+    future_time = last_timestamp + 2 * week_seconds
+
+    parameter_action = (
+        CRYPTOSWAP_OWNER_PROXY,
+        "ramp_A_gamma",
+        crypto_factory_pool.address,
+        future_A,
+        future_gamma,
+        future_time,
+    )
+
+    assert crypto_factory_pool.A() != future_A
+    assert crypto_factory_pool.gamma() != future_gamma
+
+    tx = make_vote(
+        target=CURVE_DAO_PARAM,
+        actions=[parameter_action],
+        description="test",
+        vote_creator=vote_deployer,
+    )
+
+    for log in tx.decode_logs():
+        vote_id = log.event_arguments["voteId"]
+        break
+
+    # this advances the chain one week from vote creation
+    simulate(
+        vote_id=vote_id,
+        voting_contract=CURVE_DAO_PARAM["voting"],
+    )
+
+    # need to advance another week to finish the ramp
+    ape.chain.mine(deltatime=week_seconds)
+
+    assert crypto_factory_pool.A() == future_A
+    assert crypto_factory_pool.gamma() == future_gamma
+
+
+def test_ng_ramp_parameters(vote_deployer, tricrypto_pool):
     week_seconds = 7 * 86400
     block = ape.chain.blocks[-1]
     last_timestamp = block.timestamp
@@ -58,7 +109,61 @@ def test_ramp_parameters(vote_deployer, tricrypto_pool):
     assert tricrypto_pool.gamma() == future_gamma
 
 
-def test_commit_parameters(vote_deployer, tricrypto_pool):
+def test_crypto_factory_commit_parameters(vote_deployer, crypto_factory_pool):
+    # new
+    new_mid_fee = 35000000
+    new_out_fee = 50000000
+    # same
+    new_admin_fee = 5000000000
+    new_fee_gamma = 230000000000000
+    new_allowed_extra_profit = 2000000000000
+    new_adjustment_step = 146000000000000
+    new_ma_time = 600
+
+    parameter_action = (
+        CRYPTOSWAP_OWNER_PROXY,
+        "commit_new_parameters",
+        crypto_factory_pool.address,
+        new_mid_fee,
+        new_out_fee,
+        new_admin_fee,
+        new_fee_gamma,
+        new_allowed_extra_profit,
+        new_adjustment_step,
+        new_ma_time,
+    )
+
+    assert crypto_factory_pool.mid_fee() != new_mid_fee
+    assert crypto_factory_pool.out_fee() != new_out_fee
+
+    tx = make_vote(
+        target=CURVE_DAO_PARAM,
+        actions=[parameter_action],
+        description="test",
+        vote_creator=vote_deployer,
+    )
+
+    for log in tx.decode_logs():
+        vote_id = log.event_arguments["voteId"]
+        break
+
+    # this advances the chain one week from vote creation
+    simulate(
+        vote_id=vote_id,
+        voting_contract=CURVE_DAO_PARAM["voting"],
+    )
+
+    # admin actions delay is 3 days
+    ape.chain.mine(deltatime=3 * 86400)
+    ape.Contract(CRYPTOSWAP_OWNER_PROXY).apply_new_parameters(
+        crypto_factory_pool.address, sender=vote_deployer
+    )
+
+    assert crypto_factory_pool.mid_fee() == new_mid_fee
+    assert crypto_factory_pool.out_fee() == new_out_fee
+
+
+def test_ng_commit_parameters(vote_deployer, tricrypto_pool):
     """
     TriCryptoINV
     A: 1707629
