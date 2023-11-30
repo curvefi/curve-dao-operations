ee18c38346652955ee8e8c0dd7bc0abcdd295a28
mo <anonmo427@gmail.com>
2023-11-30 17:15:30 +0100

    init

--- curve_dao/vote_utils.py
+++ curve_dao/vote_utils.py
@@ -1,4 +1,5 @@
 import warnings
+from datetime import datetime
 from typing import Dict, List, Tuple
 
 import ape
@@ -73,11 +74,29 @@ def make_vote(target: Dict, actions: List[Tuple], description: str, vote_creator
 
 
 def get_vote_script(vote_id: str, vote_type: str) -> str:
+    try:
+        voting_contract_address = get_dao_voting_contract(vote_type)
+        voting_contract = ape.project.Voting.at(voting_contract_address)
+        vote = voting_contract.getVote(vote_id)
+        script = vote["script"]
+        return script
+    except Exception:
+        return False
+
+
+def get_vote_data(vote_id: str, vote_type: str) -> str:
     voting_contract_address = get_dao_voting_contract(vote_type)
     voting_contract = ape.project.Voting.at(voting_contract_address)
-    vote = voting_contract.getVote(vote_id)
-    script = vote["script"]
-    return script
+    func = voting_contract.getVote(vote_id)
+    data = []
+    data.append(func["yea"] / 1e18)
+    data.append(func["nay"] / 1e18)
+    data.append(func["votingPower"] / 1e18)
+    data.append(func["open"])
+    data.append(func["executed"])
+    data.append(func["startDate"])
+
+    return data
 
 
 def decode_vote_script(script):
@@ -133,6 +152,60 @@ def decode_vote_script(script):
         votes.append(vote)
 
     return votes
+
+
+def format_data(data, vote_type):
+    yes = round(data[0], 2)
+    no = round(data[1], 2)
+    total_votes = data[0] + data[1]
+    total_voting_power = data[2]
+
+    # Handle edge case where there are no votes at all
+    if total_votes == 0 or total_voting_power == 0:
+        quorum = 0
+        support = 0
+    else:
+        quorum = round(total_votes / total_voting_power * 100, 2)
+        support = round(yes / total_votes * 100, 2)
+
+    required_support = 51 if vote_type == "ownership" else 30
+    required_quorum = 30 if vote_type == "ownership" else 15
+
+    if data[3]:  # Voting is ongoing
+        pass_status = "[yellow]Voting Ongoing[/]"
+    else:  # Voting is closed
+        if total_votes == 0 or total_voting_power == 0:
+            pass_status = "[red]Vote Invalid: No Votes[/]"
+        elif support >= required_support and quorum >= required_quorum:
+            # Check if the vote has been executed
+            execution_status = (
+                "[green]Executed[/]" if data[4] else "[red]Not Executed[/]"
+            )
+            pass_status = (
+                f"[green]Vote Passed[/] ([grey]Execution Status[/]: {execution_status})"
+            )
+        else:
+            if support < required_support and quorum < required_quorum:
+                failure_reason = "Both Support and Quorum Not Met"
+            elif support < required_support:
+                failure_reason = "Support Not Met"
+            else:
+                failure_reason = "Quorum Not Met"
+            pass_status = f"[red]Vote Failed: {failure_reason}[/]"
+
+    start = datetime.utcfromtimestamp(data[5]).strftime("%Y-%m-%d %H:%M:%S")
+    end = datetime.utcfromtimestamp(data[5] + 604800).strftime("%Y-%m-%d %H:%M:%S")
+
+    results_output = (
+        f"[bold]Results[/]: {pass_status}\n"
+        f" ├─ [grey]Voting Start Time[/]: {start}\n"
+        f" ├─ [grey]Voting End Time[/]: {end}\n"
+        f" ├─ [green]Votes For[/]: {yes}\n"
+        f" ├─ [red]Votes Against[/]: {no}\n"
+        f" ├─ [blue]Support[/]: {support}% (Required: {required_support}%)\n"
+        f" └─ [purple]Quorum[/]: {quorum}% (Minimum: {required_quorum}%)\n"
+    )
+    return results_output
 
 
 def get_inputs_with_names(abi, inputs):
