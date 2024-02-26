86af5cd69f73bae037cb3156c1caaf4cdeaa744a
mo <anonmo427@gmail.com>
2024-02-26 11:31:35 +0100

    script: decode executable

--- scripts/decode_executable.py
+++ scripts/decode_executable.py
@@ -1,11 +1,11 @@
 import sys
 import warnings
-
-import ape
 import click
 from rich.console import Console as RichConsole
 
+# need to find a way how to query events for ipfs hash
 from curve_dao.ipfs import get_description_from_vote_id
+
 from curve_dao.vote_utils import (
     MissingVote,
     decode_vote_data,
@@ -22,6 +22,7 @@ RICH_CONSOLE = RichConsole(file=sys.stdout)
 @click.group(
     short_help="Curve DAO proposal decoder",
 )
+
 def cli():
     """
     Command-line helper for managing Smartwallet Checker
@@ -29,19 +30,30 @@ def cli():
 
 
 @cli.command(
-    cls=ape.cli.NetworkBoundCommand,
     name="decode",
     short_help="Decode Curve DAO proposal by Vote ID",
 )
-@ape.cli.network_option()
+
 @click.option(
     "--vote-type",
     "-t",
     type=click.Choice(["ownership", "parameter"]),
     required=True,
 )
-@click.option("--vote-id", "-v", type=int, required=True)
-def decode(network, vote_type: str, vote_id: int):
+
+@click.option(
+    "--vote-id", 
+    "-v", 
+    type=int, 
+    required=True)
+
+
+def decode(vote_type: str, vote_id: int):
+    """
+    Function to decode Curve DAO votes.
+
+    Decoding with: $ python3 scripts/decode_executable.py --vote-type ownership --vote-id 100
+    """
 
     RICH_CONSOLE.log(f"Decoding {vote_type} VoteID: {vote_id}")
 
@@ -53,17 +65,26 @@ def decode(network, vote_type: str, vote_id: int):
         )
         return
 
-    description = get_description_from_vote_id(vote_id, vote_type)
-    RICH_CONSOLE.log(description)
+    # need to find a way to query events with boa to fetch the ipfs link and description of the vote.
+    try:
+        description = get_description_from_vote_id(vote_id, vote_type)
+        RICH_CONSOLE.log(description)
+    except:
+        pass
 
+    # decoding the vote script
     votes = decode_vote_script(script)
     for vote in votes:
         formatted_output = vote["formatted_output"]
         RICH_CONSOLE.log(formatted_output)
 
-    # get vote data
+    # fetching vote data
     data = get_vote_data(vote_id, vote_type)
 
-    # decode vote data
+    # decoding vote data
     results = decode_vote_data(data, vote_type)
     RICH_CONSOLE.log(results["formatted_output"])
+
+
+if __name__ == '__main__':
+    decode()
